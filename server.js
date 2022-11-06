3baf6f637810be9e611a026e5201e43b25e6860e
return0 <phoommin.naksenee@gmail.com>
2022-11-06 13:31:11 +0700

    let's go back-end mysql

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+const mysql = require('mysql2');
+
+var host = 'localhost'
+
+if (process.env.NODE_ENV == 'production') {
+    host = 'mysql-server'
+}
+
+const connection = mysql.createConnection({
+    host: host,
+    user: 'root',
+    password: '1234',
+    database: 'travel'
+});
+
+app.get('/attractions', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM `attractions`',
+        function (err, results, fields) {
+            res.json({ status: 'ok', data: results })
+        }
+    );
+})
+
+app.listen(3333, function () {
+    console.log('CORS-enabled web server listening on port 3333')
+})
